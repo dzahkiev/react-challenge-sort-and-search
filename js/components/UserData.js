56d834f5049fb3b04cf085643c641df9d4531bdb
umar-dzahkiev <egikalegi@gmail.com>
2016-12-30 11:13:15 +0300

    image link fix

--- js/components/UserData.js
+++ js/components/UserData.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default ({ item, index, update }) => {
 	return (
 		<tr className="user-data" data-id="{item.id}" onClick={() => update({active : index})} >
-			<td><img src={'/images/'+item.image+'.svg'} className="image" /></td>
+			<td><img src={'images/'+item.image+'.svg'} className="image" /></td>
 			<td>{item.name}</td>
 			<td>{item.age}</td>
 			<td>{item.phone}</td>
